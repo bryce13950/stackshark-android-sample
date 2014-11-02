d8e3836c0de1101dbc1b3f75aa0d527b12154e5f
bryce meyer <brycemeyer@bryces-Air.local>
2014-11-02 14:09:08 -0600

    initial commit

---
+++ app/src/main/java/com/axolotlinteractive/stackshark/andorid/reporter/ErrorReporter.java
@@ -0,0 +1,19 @@
+package com.axolotlinteractive.stackshark.andorid.reporter;
+
+/**
+ * Created by brycemeyer on 11/2/14.
+ */
+public class ErrorReporter
+{
+    private static String ProjectKey;
+
+    public static void setProjectKey(String projectKey)
+    {
+        ProjectKey = projectKey;
+    }
+
+    public static void init()
+    {
+        //TODO initialize error handling
+    }
+}
